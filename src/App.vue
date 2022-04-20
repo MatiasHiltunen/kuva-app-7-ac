8edaf51172f5e9a435bbb2a2c7f02bd0b50ba2ae
MatiasHiltunen <matias.s.j@hotmail.com>
2022-04-20 17:02:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { RouterView } from 'vue-router'
+import NavigationBar from './components/navigation/NavigationBar.vue';
+
+</script>
+
+<template>
+ <NavigationBar></NavigationBar>
+ <router-view></router-view>
+</template>
+
+<style>
+
+
+</style>
---
+++ src/components/publication/PublicationCreate.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed, reactive } from 'vue'
+
+
+const publicationData = reactive({
+    title: '',
+    description: '',
+    url: '',
+    visibility: 2, // Julkinen postaus
+    tags: []
+})
+
+
+const isDataValid = computed(()=>{
+    return !!(publicationData.title && publicationData.description && publicationData.url)
+})
+
+
+const createNewPublication = ()=>{
+
+    publicationData.title = ''
+    publicationData.description = ''
+    publicationData.url = ''
+
+}
+
+
+</script>
+
+<template>
+
+    <div>
+        <label>Otsikko</label>
+        <input v-model="publicationData.title" type="text">
+
+        <label>Kuvaus</label>
+        <input v-model="publicationData.description" type="text">
+
+        <label>URL</label>
+        <input v-model="publicationData.url" type="text">
+
+        <button :disabled="!isDataValid" @click="createNewPublication">Lähetä</button>
+
+    </div>
+
+  
+
+</template>
