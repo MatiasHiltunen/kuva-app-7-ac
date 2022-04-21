634211c60033e66c303b255346d11ceb600d5a24
Matias Hiltunen <matias.s.j@hotmail.com>
2022-04-21 15:04:57 +0300

    add validations

--- src/components/publication/PublicationCreate.vue
+++ src/components/publication/PublicationCreate.vue
@@ -11,12 +11,24 @@ const publicationData = reactive({
 })
 
 
-const isDataValid = computed(()=>{
-    return !!(publicationData.title && publicationData.description && publicationData.url)
+const isDataValid = computed(() => {
+
+    const urlValidation = publicationData.url.includes('https://')
+    const descriptionValidation = publicationData.description.length < 1000
+    const titleValidation = publicationData.title.length > 2
+
+    return {
+        urlValidation: urlValidation ? 'OK' : 'Vain https osoitteet ovat sallittu',
+        descriptionValidation: descriptionValidation ? 'OK' : 'Kuvauksen teksti on liian pitkä',
+        titleValidation: titleValidation ? 'OK' : 'Otsikon täytyy olla ainakin kolme merkkiä pitkä',
+        isAllValid: urlValidation && descriptionValidation && titleValidation
+    }
 })
 
 
-const createNewPublication = ()=>{
+const createNewPublication = () => {
+
+    if (!isDataValid.value.isAllValid) return
 
     publicationData.title = ''
     publicationData.description = ''
@@ -31,18 +43,20 @@ const createNewPublication = ()=>{
 
     <div>
         <label>Otsikko</label>
+        <small>{{ isDataValid.titleValidation }}</small>
         <input v-model="publicationData.title" type="text">
 
+        <small>{{ isDataValid.descriptionValidation }}</small>
         <label>Kuvaus</label>
         <input v-model="publicationData.description" type="text">
 
+        <small>{{ isDataValid.urlValidation }}</small>
         <label>URL</label>
         <input v-model="publicationData.url" type="text">
 
-        <button :disabled="!isDataValid" @click="createNewPublication">Lähetä</button>
-
+        <button :disabled="!isDataValid.isAllValid" @click="createNewPublication">Lähetä</button>
     </div>
 
-  
+
 
 </template>
